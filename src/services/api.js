f413c9cc4ef2735434e61bd20d443f0121c1d2f2
Randolh <estuardotecun@gmail.com>
2026-06-18 15:04:32 -0600

    feat: :zap: Add categories array to api service for game filtering

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 const BASE_URL = 'https://free-to-play-games-database.p.rapidapi.com/api'
 export const IMAGE_PROXY_URL = 'https://wsrv.nl/?url='
 
+const categories = [
+    'mmorpg', 'shooter', 'strategy', 'moba', 'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival', 'pvp', 'pve', 'pixel', 'voxel', 'zombie', 'turn-based', 'first-person', 'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller', 'superhero', 'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps', 'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci-fi', 'fighting', 'action-rpg', 'action', 'military', 'martial-arts', 'flight', 'low-spec', 'tower-defense', 'horror', 'mmorts'
+]
+
 export const api = {
     async request(endpoint, options = {}) {
         try {
